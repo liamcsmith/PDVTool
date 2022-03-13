554b48453f110b81e4e62799fb3dda9c47a3c28f
Liam Smith <liam.smith@eng.ox.ac.uk>
2022-03-13 19:09:40 +0000

    Adding documentation

--- PdvTrace.m
+++ PdvTrace.m
@@ -1,39 +1,76 @@
 classdef PdvTrace
-%%%  An object for containing everything about a PDV trace. It will manage raw data storage (using ScopeTrace dependency), analysis via its interface with the bundled PDVAnalysis GUI (including analysis parameter storage, and analysis updating from prior analyses) and cable delay inputs.
+% PdvTrace:  An object for containing everything about a PDV trace.
 % 
-% ## Dependencies
-% **ScopeTrace.m** - You can find this in a different repository on my profile.
+% This object will manage raw data storage (using ScopeTrace dependency),
+% analysis via its interface with the bundled PDVAnalysis GUI 
+% (including analysis parameter storage, and analysis updating from prior 
+% analyses) and cable delay inputs. 
+%
+% PdvTrace: Dependencies
 % 
-% ## Installation
-% To have PDVTrace work correctly you will need to:
-% 1) Edit the path on Line 24 of PDVTrace.m to give a suitable path to ScopeTrace.m, if you do not do this the PDVTrace object will not be able to read in data from the raw oscilloscope file!
-% 2) edit the path on Line 92 of PDVAnalysis.m to give a suitable path to ScopeTrace.m, if you do not do this the GUI will not be able to read in data from raw data passed in via "Trace", nor will it be able to import raw data if launched without raw data input arguments!
+%   ScopeTrace - You can find this in a different repository on my profile.
 % 
-% ## InputArgs
-% (all passed as Name-Value pairs, all optional [^1])
-% [^1]: If you do not pass a FilePath then the object will use ScopeTrace to launch a file explorer to select and import a raw oscilloscope trace.
-% 
-% | Name          	| DataType      | Default      	| Description   											|
-% | ------------- 	| ------------- | -------------	| ------------- 											|
-% | "FilePath"    	| string        | N/A          	| Absolute or relative file path to a raw oscilloscope file. This will be imported using ScopeTrace 	|
-% | "AnalysisParameters" 	| struct[^3] 	| N/A        	| The analysis parameter struct that is outputted from the PDVAnalysis GUI. 				|
-% | "Delay" 		| numeric 	| 0.0 		| The cable delay asscoiated with this trace (including fiber & PDV channel delays) given in seconds. 	|
-% | "Title" 		| string 	| 'Generic' 	| A title for the associated PDV Trace, passed to PDVAnalysis GUI for its UIFigure title 		|
-% | "ProbeWavelengthNM" 	| numeric 	| 1550[^4] 	| The wavelength (in nm) of the probe laser.								|
-% [^3]: I wouldn't worry about this field too much, PDV Trace will save the analysis parameters in a cache alongside the raw data file and then automatically repopulate this when you pass it a raw data file (that has an associated analysis cache file.
-% [^4]: This sets the velocity scale and its important you get it correct (so note down when doing experiments). The default is set at 1550 (which I use always so as to avoid issues if i forget it).
+% PdvTrace: Installation
+%
+%   In order to use this class correctly you should:
+%   
+%   1 - Edit the value for the property ScopeTracePath. To find this you
+%       can use the MATLAB documentation hyperlink or find 
+%       %%ScopeTracePath%% in the PdvTrace.m file.
+%   
+%   2 - Edit the value for the property ScopeTracePath in PdvAnalysis.m. 
+%       To find this you can use the MATLAB documentation hyperlink (in the
+%       PdvAnalysis documentation) or find %%ScopeTracePath%% in
+%       the PdvAnalysis.m file.
+%   
+%   Note: If you do not do these steps this object will not be able to 
+%         interface with raw data properly.
+%
+% PdvTrace: Constructor Arguments (All given as Name Value Pairs):
+%
+%   "FilePath" - Absolute or relative file path to a raw oscilloscope file.
+%                This will be imported using ScopeTrace.
+%                               
+%   "AnalysisParameters" - The analysis parameter struct that is outputted 
+%                          from the PDVAnalysis GUI. [1]
+%
+%   "Delay" - The cable delay asscoiated with this trace (including fiber &
+%              PDV channel delays) given in seconds. Default = 0.0
+%                               
+%   "Title" - A title for the associated PDV Trace, passed to PDVAnalysis
+%             GUI for its UIFigure title.
+%                               
+%   "ProbeWavelengthNM" - The wavelength (in nm) of the probe laser. [2]
+%                         Default = 1550
+%   
+%   Note: If run without arguments ScopeTrace will launch a file selection
+%         diaglogue.
+%
+% See also ScopeTrace
+
     properties
-        RawTrace
-        Title
-        AnalysisParameters
-        Delay
-        ProcessedTrace
-        ProbeWavelengthNM
+        % RawTrace - This handles the raw data storage of the oscilloscope file. Including storage of any metadata and the ability to plot the raw data quickly.
+        RawTrace ScopeTrace
+        % Title - A title for the associated PDV Trace, passed to the PdvAnalysis GUI for its UIFigure title when using the Analyse() method.
+        Title {mustBeText}
+        % AnalysisParameters - The analysis parameter struct that is outputted from the PDVAnalysis GUI upon a successful analysis.
+        AnalysisParameters struct
+        % Delay - The cable delay asscoiated with this trace (including fiber & PDV channel delays) given in seconds.
+        Delay {mustBeNumeric}
+        % ProcessedTrace - This struct contains the results of a succesful PdvAnalysis. 
+        ProcessedTrace struct
+        % ProbeWavelengthNM - The wavelength (in nm) of the probe laser.
+        ProbeWavelengthNM {mustBeNumeric}
+        % ScopeTracePath - A valid path to a folder containing ScopeTrace, this must be correctly set for the script to work. If you set the default in the .m file it does not need setting each time.
+        ScopeTracePath {mustBeFolder} = '~/Documents/GitHub/ImportScope'  %%ScopeTracePath%%
     end
     properties (Dependent)
-        Time
-        Velocity
-        Error
+        % Column vector containing time values for the analysed PDV trace.
+        Time {mustBeNumeric}
+        % Column vector containing velocity values for the analysed PDV trace.
+        Velocity {mustBeNumeric}
+        % Column vector containing velocity error values for the analysed PDV trace.
+        Error {mustBeNumeric}
     end
     properties (Dependent,Access=private)
         StartTimeUs
@@ -43,34 +80,65 @@ classdef PdvTrace
         StartFreqGHz
         EndFreqGHz
     end
-    properties (Access=private)
-        ScopeTracePath = '~/Documents/GitHub/ImportScope' % CHANGE ME TO SATISFY DEPENDENCY
-    end
-   
     methods
-        function obj = PdvTrace(inputargs)
-            arguments
-                inputargs.FilePath              = 'Undefined';
-                inputargs.AnalysisParameters    = 'Undefined';
-                inputargs.Delay                 = 0;
-                inputargs.ProcessedTrace        = 'Not Calculated';
-                inputargs.Title                 = 'Generic PDV Trace'
-                inputargs.ProbeWavelengthNM     = 1550;
+        function obj        = PdvTrace(InputArgs)
+            %Valid Arguments - {"FilePath",  "AnalysisParameters",  "Delay",  "Title",  "ProbeWavelengthNM"}
+            % 
+            % INPUT "FilePath" - Absolute or relative file path to a raw 
+            %                    oscilloscope file. This will be imported 
+            %                    using ScopeTrace.
+            %                               
+            %       "AnalysisParameters" - The analysis parameter struct
+            %                              that is outputted from the
+            %                              PDVAnalysis GUI. [1]
+            %                               
+            %       "Delay" - The cable delay asscoiated with this trace
+            %                 (including fiber & PDV channel delays) given
+            %                 in seconds. Default = 0.0
+            %                               
+            %       "Title" - A title for the associated PDV Trace, passed
+            %                 to PDVAnalysis GUI for its UIFigure title.
+            %                               
+            %       "ProbeWavelengthNM" - The wavelength (in nm) of the
+            %                             probe laser. [2] Default = 1550
+            %
+            % OUTPUT  obj - The object.
+            %
+            % REMARKS   1) I wouldn't worry about this field too much,
+            %              PDV Trace will save the analysis parameters in a
+            %              cache alongside the raw data file and then
+            %              automatically repopulate this when you pass it
+            %              a raw data file (that has an associated
+            %              analysis cache file.
+            %           
+            %           2) This sets the velocity scale and its important
+            %               you get it correct (so note down when doing
+            %               experiments). The default is set at 1550
+            %               (which I use always so as to avoid issues if
+            %               I forget it).
+            %
+            arguments         
+                InputArgs.FilePath              = 'Undefined'; 
+                InputArgs.AnalysisParameters    = 'Undefined';
+                InputArgs.Delay                 = 0;
+                InputArgs.ProcessedTrace        = 'Not Calculated';
+                InputArgs.Title                 = 'Generic PDV Trace'
+                InputArgs.ProbeWavelengthNM     = 1550;
             end
             obj.CheckDependency
             
-            switch inputargs.FilePath
+            switch InputArgs.FilePath
                 case 'Undefined'
                     obj.RawTrace    = ScopeTrace;
                 otherwise
                     obj.RawTrace    = ScopeTrace('FilePath', ...
-                                                 inputargs.FilePath);
+                                                 InputArgs.FilePath);
             end
-            obj.AnalysisParameters  = inputargs.AnalysisParameters;
-            obj.Delay               = inputargs.Delay;
-            obj.ProcessedTrace      = inputargs.ProcessedTrace;
-            obj.Title               = inputargs.Title;
-            obj.ProbeWavelengthNM   = inputargs.ProbeWavelengthNM;
+            obj.AnalysisParameters  = InputArgs.AnalysisParameters;
+            obj.Delay               = InputArgs.Delay;
+            obj.ProcessedTrace      = InputArgs.ProcessedTrace;
+            obj.Title               = InputArgs.Title;
+            obj.ProbeWavelengthNM   = InputArgs.ProbeWavelengthNM;
             clearvars inputargs
             
             % Loading Saved if it exists
@@ -91,7 +159,21 @@ classdef PdvTrace
                 end
             end
         end
-        function obj = Analyse(obj)
+        function obj        = Analyse(obj)
+            %Analyse Run analysis on stored data using PDVAnalysis GUI.
+            %
+            % When using this method if you want to store the analysis
+            % within scope trace you should click Return&Close when you've
+            % finished with the GUI and are happy with the trace. Note that
+            % if a ProcessedTrace already exists within the object it will
+            % use the parameters from this trace as the starting state of
+            % the GUI.
+            %
+            % INPUT   obj - The object.
+            % 
+            % OUTPUT  obj - The object, with an updated ProcessedData 
+            %               and AnalysisParameter properties.
+            %
             obj.CheckDependency
             ParentFunctionInterfacingHandle = @ParentFunctionPullOutputs;
             if isstruct(obj.AnalysisParameters)
@@ -142,7 +224,19 @@ classdef PdvTrace
                 end
             end
         end
-        function obj = ResetAnalysis(obj)
+        function obj        = ResetAnalysis(obj)
+            %ResetAnalysis Remove a ProcessedTrace and clear the associated cache file.
+            %
+            % This method will remove any processed data and analysis 
+            % parameters from the object, as well as deleting the cache of 
+            % the processed data being deleted.
+            %
+            % INPUT   obj - The object.
+            % 
+            % OUTPUT  obj - The object, with freshly emptied 
+            %               ProcessedTrace and AnalysisParameters
+            %               properties.
+
             obj.AnalysisParameters = 'Not Defined';
             obj.ProcessedTrace     = 'Not Calculated';
             
@@ -152,7 +246,19 @@ classdef PdvTrace
                 delete(filepath)
             end
         end
-        function       AnalysisSummary(obj)
+        function              AnalysisSummary(obj)
+            %AnalysisSummary Display analysis properties of ProcessedTrace.
+            %
+            % This method will display the analysis properties for the
+            % ProcessedTrace stored within the object.
+            %
+            % INPUT   obj - The object.
+            % 
+            % OUTPUT  none - Output is not assignable, this functions
+            %                purely to display the analysis properties, if
+            %                you wish to access the properties see the
+            %                AnalysisParameters field within the
+            %                ProcessedTrace object property.
             disp('PDV Trace Analysis:')
             disp(['Start Time [us]: ',obj.StartTimeUs])
             disp(['End Time [us]: ',obj.EndTimeUs])
@@ -161,8 +267,6 @@ classdef PdvTrace
             disp(['Window Size [Samples]: ',obj.WindowSize])
             disp(['Overlap [Samples]: ',obj.Overlap])
         end
-    end
-    methods
         function Time       = get.Time(         obj)
             if ~isnan(obj.Delay)
                 Time = obj.ProcessedTrace.Time - obj.Delay;
@@ -218,6 +322,8 @@ classdef PdvTrace
                 Overlap = 'Undefined';
             end
         end
+    end
+    methods (Access=private)
         function              CheckDependency(  obj)
             if ~exist('ScopeTrace','file')
                 addpath(obj.ScopeTracePath);
--- README.md
+++ README.md
@@ -1,54 +1,78 @@
 # PDVTrace
 ## Summary
-An object for containing everything about a PDV trace. It will manage raw data storage (using ScopeTrace dependency), analysis via its interface with the bundled PDVAnalysis GUI (including analysis parameter storage, and analysis updating from prior analyses) and cable delay inputs.
+An object for containing everything about a PDV trace. This object will manage raw data storage (using ScopeTrace dependency), analysis via its interface with the bundled PDVAnalysis GUI (including analysis parameter storage, and analysis updating from prior analyses) and cable delay inputs.
 
 ## Dependencies
-**ScopeTrace.m** - You can find this in a different repository on my profile.
+* **ScopeTrace.m**: You can find this in a different repository on my profile.
 
 ## Installation
 To have PDVTrace work correctly you will need to:
-1) Edit the path on Line 24 of PDVTrace.m to give a suitable path to ScopeTrace.m, if you do not do this the PDVTrace object will not be able to read in data from the raw oscilloscope file!
-2) edit the path on Line 92 of PDVAnalysis.m to give a suitable path to ScopeTrace.m, if you do not do this the GUI will not be able to read in data from raw data passed in via "Trace", nor will it be able to import raw data if launched without raw data input arguments!
+1) Edit the value for the property ScopeTracePath. To find this you can use the MATLAB documentation hyperlink or find %%ScopeTracePath%% in the PdvTrace.m file.
+2) Edit the value for the property ScopeTracePath in PdvAnalysis.m. To find this you can use the MATLAB documentation hyperlink (in the PdvAnalysis documentation) or find %%ScopeTracePath%% in the PdvAnalysis.m file.
 
-## InputArgs
+## Constructor Aguments
 (all passed as Name-Value pairs, all optional [^1])
 [^1]: If you do not pass a FilePath then the object will use ScopeTrace to launch a file explorer to select and import a raw oscilloscope trace.
 
-| Name          	| DataType      | Default      	| Description   											|
-| ------------- 	| ------------- | -------------	| ------------- 											|
-| "FilePath"    	| string        | N/A          	| Absolute or relative file path to a raw oscilloscope file. This will be imported using ScopeTrace 	|
-| "AnalysisParameters" 	| struct[^3] 	| N/A        	| The analysis parameter struct that is outputted from the PDVAnalysis GUI. 				|
-| "Delay" 		| numeric 	| 0.0 		| The cable delay asscoiated with this trace (including fiber & PDV channel delays) given in seconds. 	|
-| "Title" 		| string 	| 'Generic' 	| A title for the associated PDV Trace, passed to PDVAnalysis GUI for its UIFigure title 		|
-| "ProbeWavelengthNM" 	| numeric 	| 1550[^4] 	| The wavelength (in nm) of the probe laser.								|
+| Name          		| DataType   | Default      | Description   																						|
+| --------------------- | ---------- | ------------ | ----------------------------------------------------------------------------------------------------- |
+| "FilePath"    		| string     | N/A          | Absolute or relative file path to a raw oscilloscope file. This will be imported using ScopeTrace 	|
+| "AnalysisParameters" 	| struct[^3] | N/A        	| The analysis parameter struct that is outputted from the PDVAnalysis GUI. 							|
+| "Delay" 				| numeric 	 | 0.0 			| The cable delay associated with this trace (including fibre & PDV channel delays) given in seconds.	|
+| "Title" 				| string 	 | 'Generic'	| A title for the associated PDV Trace, passed to PDVAnalysis GUI for its UIFigure title 				|
+| "ProbeWavelengthNM" 	| numeric 	 | 1550[^4] 	| The wavelength (in nm) of the probe laser.															|
+
 [^3]: I wouldn't worry about this field too much, PDV Trace will save the analysis parameters in a cache alongside the raw data file and then automatically repopulate this when you pass it a raw data file (that has an associated analysis cache file.
 [^4]: This sets the velocity scale and its important you get it correct (so note down when doing experiments). The default is set at 1550 (which I use always so as to avoid issues if i forget it).
 
+## Properties
+
+| PropertyName 			| DataType 		| Summary 																														|
+| ----------------- 	| ------------- | ----------------------------------------------------------------------------------------------------------------------------- |
+| RawTrace 				| ScopeTrace 	| This handles the raw data storage of the oscilloscope file using a ScopeTrace object.											|
+| Title 				| string 		| A title for the associated PDV Trace, passed to the PdvAnalysis GUI for its UIFigure title when using the Analyse() method.	|
+| AnalysisParameters	| struct[^3]	| The analysis parameter struct that is outputted from the PDVAnalysis GUI upon a successful analysis.							|
+| Delay 				| numeric	 	| The cable delay associated with this trace (including fibre & PDV channel delays) given in seconds.							|
+| ProcessedTrace 		| struct 		| This struct contains the results of a successful PdvAnalysis. 																	|
+| ProbeWavelengthNM 	| numeric 		| The wavelength (in nm) of the probe laser.																					|
+| ScopeTracePath 		| string	 	| A valid path to a folder containing ScopeTrace. 																				|
+| Time 			 		| numeric array	| Column vector containing time values for the analysed PDV trace. 																|
+| Velocity 		 		| numeric array	| Column vector containing velocity values for the analysed PDV trace.		 													|
+| Error			 		| numeric array	| Column vector containing velocity error values for the analysed PDV trace.
+
+## Methods
+
+| MethodName 		| Input 			| Output 																					| Summary 														|
+| ----------------- | ----------------- | ----------------------------------------------------------------------------------------- | ------------------------------------------------------------- |
+| Analyse 			| obj - The object. | obj - The object, with updated ProcessedData and AnalysisParameters properties. 			| Run analysis on stored data using PDVAnalysis GUI.			|
+| ResetAnalysis 	| obj - The object. | obj - The object, with empty ProcessedData and AnalysisParameters properties. 			| Remove a ProcessedTrace and clear the associated cache file.	|
+| AnalysisSummary	| obj - The object. | none - Output is not assignable, this functions purely to display the analysis properties | Print the AnalysisParameters used to create ProcessedTrace.	|
 
 # PdvAnalysis
 ## Summary
 An extensive GUI tool for analysing photon doppler velocimetry data within MATLAB. It is highly recommended that you use this app as a tool within the above PDVTrace object. PDVTrace is designed to manage the data that PDVAnalysis produces, allowing the user to complete,save,revisit, and summarise analyses all whilst effectively minimised memory allocation. Should you specifically wise to use PDVAnalysis outside of this wrapper please see below.
 
 ## Dependencies
-**ScopeTrace.m**: You can find this in a different repository on my profile.
+* **ScopeTrace.m**: You can find this in a different repository on my profile.
 
 ## Installation
-To have PDVAnalysis work correctly you will need to edit the path on Line 92 to give a suitable path to ScopeTrace.m, if you do not do this the GUI will not be able to read in data from raw data passed in via "Trace", nor will it be able to import raw data if launched without raw data input arguments!  
+To have PDVAnalysis work correctly you will need to:
+ 1) Edit the value for the property ScopeTracePath in PdvAnalysis.m. To find this you can use the MATLAB documentation hyperlink or find %%ScopeTracePath%% in the PdvAnalysis.m file.
 
 ## InputArgs
 (all passed as Name-Value pairs, all optional [^2])
 [^2]: If you do not pass a Trace or both Time & Voltage arguments then the GUI will use ScopeTrace to launch a file explorer to select and import a raw oscilloscope trace.
 
-| Name          	| DataType      | Default       | Description   											|
-| ------------- 	| ------------- | ------------- | ------------- 											|
-| "Time"   		| numeric array | []   		| A 1D array with time values for the PDV trace being analysed. 					|
-| "Voltage"     	| numeric array	| [] 		| A 1D array with photodiode voltage values for the PDV trace being analysed. 				|
-| "ProbeWavelengthNM" 	| numeric 	| 1550[^4] 	| The wavelength (in nm) of the probe laser. 								|
-| "Trace"		| ScopeTrace	| N/A 		| An outputted ScopeTrace object from ScopeTrace.m[^5] 							|
-| "Parameters"  	| struct 	| N/A 		| This is the analysis parameter struct that is outputted from the PDVAnalysis GUI.[^6] 		|
-| "Automate"		| logical 	| false		| If true the GUI analyses the raw data with the associated parameters[^7] 				|
-| "Title"		| string	| 'Generic" 	| A title for the associated PDV Trace, this will be used as the UIFigure title 			|
-| "ParentApp" 		|function_handle| N/A 		| **Warning ADVANCED** This input allows you to attach the GUI to a different app or function [^8]	| 
+| Name          		| DataType      | Default       | Description   																					|
+|-----------------------|---------------|---------------|---------------------------------------------------------------------------------------------------|
+| "Time"   				| numeric array | []   			| A 1D array with time values for the PDV trace being analysed. 									|
+| "Voltage"     		| numeric array	| [] 			| A 1D array with photodiode voltage values for the PDV trace being analysed. 						|
+| "ProbeWavelengthNM" 	| numeric 		| 1550[^4] 		| The wavelength (in nm) of the probe laser. 														|
+| "Trace"				| ScopeTrace	| N/A 			| An outputted ScopeTrace object from ScopeTrace.m[^5] 												|
+| "Parameters"  		| struct 		| N/A 			| This is the analysis parameter struct that is outputted from the PDVAnalysis GUI.[^6] 			|
+| "Automate"			| logical 		| false			| If true the GUI analyses the raw data with the associated parameters[^7] 							|
+| "Title"				| string		| 'Generic" 	| A title for the associated PDV Trace, this will be used as the UIFigure title 					|
+| "ParentApp" 			|function_handle| N/A 			| **Warning ADVANCED** This input allows you to attach the GUI to a different app or function [^8]	|
 [^5]: This object contains a property inside the object that will be used to provide Time/Voltage pairs.
 [^6]: I wouldn't worry about this field too much, its primarily used by PDVTrace to re-enter analyses using cached data alongside the raw data file.
 [^7]: This is very handy for reanalysing data (where you saved the prior analysis struct). PDVTrace.Analyse uses this flag when it already has got analysis parameters, allowing you to see the prior analysis before re-analysing.
